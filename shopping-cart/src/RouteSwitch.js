8a34b8932832a35e5838479a43a0a398aad8a735
keithching <keithching91@gmail.com>
2021-12-24 23:34:40 +0800

    3rd commit

--- shopping-cart/src/RouteSwitch.js
+++ shopping-cart/src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './Home';
 import Shop from './Shop';
@@ -6,14 +6,14 @@ import Shop from './Shop';
 const RouteSwitch = () => {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route index element={<Home />} />
                     <Route path="shop" element={<Shop />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 
 };
